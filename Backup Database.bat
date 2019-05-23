35f391c4b049189ac24a16658dd4bdfb007c97ab
muhieddine alqousi <alqousi.muhieddine@gmail.com>
2019-05-23 14:28:07 +0200

    V3

--- Backup Database.bat
+++ Backup Database.bat
@@ -1,1 +1,2 @@
-"C:\Program Files\PostgreSQL\10\bin\pg_dump.exe" -U postgres Statistics >  "D:\AIRCOM6 Backup\Statistics.bak"
+"C:\Program Files\PostgreSQL\10\bin\pg_dump.exe" -U postgres -n '"Daily"' Statistics >  "D:\AIRCOM6 Backup\Statistics.bak"
+pause
